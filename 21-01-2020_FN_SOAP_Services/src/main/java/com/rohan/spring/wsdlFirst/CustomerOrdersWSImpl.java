0c4eb5db8e64dc8a457f4ddf6ceb6c31cf958084
RohanSanjayPawar <rohan.sanjaypawar@accoliteindia.com>
2020-01-26 09:54:34 +0530

    SOAP service for updating and deleting a customer order

---
+++ 21-01-2020_FN_SOAP_Services/src/main/java/com/rohan/spring/wsdlFirst/CustomerOrdersWSImpl.java
@@ -0,0 +1,99 @@
+package com.rohan.spring.wsdlFirst;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cxf.feature.Features;
+
+import com.akash.ws.trainings.CreateOrdersRequest;
+import com.akash.ws.trainings.CreateOrdersResponse;
+import com.akash.ws.trainings.CustomerOrdersPortType;
+import com.akash.ws.trainings.DeleteOrdersRequest;
+import com.akash.ws.trainings.DeleteOrdersResponse;
+import com.akash.ws.trainings.GetOrdersRequest;
+import com.akash.ws.trainings.GetOrdersResponse;
+import com.akash.ws.trainings.Order;
+import com.akash.ws.trainings.Product;
+import com.akash.ws.trainings.UpdateOrdersRequest;
+import com.akash.ws.trainings.UpdateOrdersResponse;
+
+@Features(features="org.apache.cxf.feature.LoggingFeature")
+public class CustomerOrdersWSImpl implements CustomerOrdersPortType{
+
+	Map<BigInteger,List<Order>> customerOrders=new HashMap<>();
+	int currenntId;
+	
+	public CustomerOrdersWSImpl() {
+		init();
+	}
+	
+	public void init() {
+		List<Order> orders=new ArrayList<>();
+		Order order = new Order();
+		order.setId(BigInteger.valueOf(1));
+		
+		Product product=new Product();
+		product.setId("1");
+		product.setDescription("Amul");
+		product.setQuantity(BigInteger.valueOf(3));
+		order.getProduct().add(product);
+		orders.add(order);
+		
+		customerOrders.put(BigInteger.valueOf(++currenntId), orders);
+	}
+	
+	@Override
+	public GetOrdersResponse getOrders(GetOrdersRequest request) {
+		BigInteger customerId=request.getCustomerId();
+		List<Order> orders = customerOrders.get(customerId);
+		GetOrdersResponse response=new GetOrdersResponse();
+		response.getOrder().addAll(orders);
+		return response;
+	}
+
+	@Override
+	public CreateOrdersResponse createOrders(CreateOrdersRequest request) {
+		BigInteger customerId=request.getCustomerId();
+		Order order=request.getOrder();
+		List<Order> orders=customerOrders.get(customerId);
+		orders.add(order);
+		CreateOrdersResponse response=new CreateOrdersResponse();
+		response.setResult(true);
+ 		return response;
+	}
+
+	@Override
+	public DeleteOrdersResponse deleteOrders(DeleteOrdersRequest request) {
+		BigInteger customerId = request.getCustomerId();
+		// The order that must be removed
+		BigInteger orderId = request.getOrderId();
+		List<Order> orders=customerOrders.get(customerId);
+		orders.remove(orderId);
+		customerOrders.remove(customerId);
+		customerOrders.put(customerId, orders);
+		DeleteOrdersResponse response = new DeleteOrdersResponse();
+		response.setResult(true);
+		return response;
+	}
+
+	@Override
+	public UpdateOrdersResponse updateOrders(UpdateOrdersRequest request) {
+		BigInteger customerId=request.getCustomerId();
+		BigInteger orderId = request.getOrderId();
+		Order order=request.getOrder();
+		// Update
+		List<Order> orders=customerOrders.get(customerId);
+		// Removing the old order using orderId and updating the list with the new order
+		orders.remove(orderId);
+		orders.add(order);
+		customerOrders.remove(customerId);
+		customerOrders.put(customerId, orders);
+		UpdateOrdersResponse response = new UpdateOrdersResponse();
+		response.setResult(true);
+		return response;
+	}
+
+}
